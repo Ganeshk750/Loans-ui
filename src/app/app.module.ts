dafab9f0dcbb0b2534a9ca316e374a4d329769a3
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-02-07 20:36:27 +0530

    adding new project

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddToCartComponent } from './shared/components/add-to-cart/add-to-cart.component';
+import { AlertComponent } from './shared/components/alert/alert.component';
+import { ApplicationErrorComponent } from './shared/components/application-error/application-error.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+import { ResourceNotFoundComponent } from './shared/components/resource-not-found/resource-not-found.component';
+//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddToCartComponent,
+    AlertComponent,
+    ApplicationErrorComponent,
+    PageNotFoundComponent,
+    ResourceNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    //NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
