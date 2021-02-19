a153a3f1b1a059b07a45f6e94defd522435909ca
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-02-19 13:33:26 +0530

    # renaming app name to configuration file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Test-Do-App'`, () => {
+  it(`should have as title 'shop-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Test-Do-App');
+    expect(app.title).toEqual('shop-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Test-Do-App app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('shop-ui app is running!');
   });
 });
